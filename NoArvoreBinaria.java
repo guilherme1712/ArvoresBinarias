222b13f99932bae67d98372c4cca4a0426885ca7
guilherme1712 <101009229+guilherme1712@users.noreply.github.com>
2023-09-26 16:55:26 -0300

    Add files via upload

--- NoArvoreBinaria.java
+++ NoArvoreBinaria.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package arvores.binarias;
 
-/**
- *
- * @author Guilherme
- */
-public class NoArvoreBinaria {
-    
+// Classe para representar um nó da árvore binária
+class NoArvoreBinaria {
+    int dado;                   // Valor armazenado no nó
+    NoArvoreBinaria esquerda;   // Referência para o filho esquerdo
+    NoArvoreBinaria direita;    // Referência para o filho direito
+
+    public NoArvoreBinaria(int dado) {
+        this.dado = dado;
+        this.esquerda = null;
+        this.direita = null;
+    }
 }
